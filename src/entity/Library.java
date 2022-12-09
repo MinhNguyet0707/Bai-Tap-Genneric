0c9f0bc9593a3290a6e8ebed44883016597fc761
MinhNguyet0707 <minhnguyettkh@gmail.com>
2022-12-09 14:54:23 +0700

    bai tap genneric

--- src/entity/Library.java
+++ src/entity/Library.java
@@ -1,12 +1,13 @@
 package entity;
+
 import java.util.List;
 
 public class Library {
 	private List<Book> books;
 	private List<Reader> readers;
-	
+
 	public Library(List<Book> books, List<Reader> readers) {
-		
+
 		this.books = books;
 		this.readers = readers;
 	}
@@ -15,8 +16,5 @@ public class Library {
 	public String toString() {
 		return "Library [books=" + books + ", readers=" + readers + "]";
 	}
-	
-	
-	
 
 }
--- src/handle/Bookhanle.java
+++ src/handle/Bookhanle.java
@@ -1,39 +1,38 @@
 package handle;
+
 import java.util.List;
 
 import entity.Book;
 import entity.HamNhap;
 
-public class Bookhanle implements HamNhap<Book>{
+public class Bookhanle implements HamNhap<Book> {
 
 	@Override
 	public void insert(Book object, List<Book> objs) {
 		objs.add(object);
-		
+
 	}
 
 	@Override
 	public void delete(int id, List<Book> objs) {
 		for (Book o1 : objs) {
-			if(o1.getId()== id) {
-				objs.remove(o1);	
+			if (o1.getId() == id) {
+				objs.remove(o1);
 			}
-			
+
 		}
 	}
 
 	@Override
 	public void update(Book object, List<Book> objs) {
-	for (Book o : objs) {
-		if(o.getId()==object.getId()) {
-			o.setName(object.getName());
-			o.setAuthor(object.getAuthor());
-			o.setDate(object.getDate());	
+		for (Book o : objs) {
+			if (o.getId() == object.getId()) {
+				o.setName(object.getName());
+				o.setAuthor(object.getAuthor());
+				o.setDate(object.getDate());
+			}
+			break;
 		}
-		break;
-	}
-	
-	
 
-}
+	}
 }
